4b3e5327a97330a2d159dd4a42fc8650eb597ad0
khiri <abdelohabk@gmail.com>
2024-05-01 13:45:47 +0200

    ascii-art is done

--- main.go
+++ main.go
@@ -4,15 +4,19 @@ func PrintCharachters(s, FromFile string) { // "\nHello\n\nworld" // "\n\n\n"   
 	size_s := len(s) ; activeIndex := 0 ; line := 1 ; startIndex := 0 ; Charachters := Data.LoadDataFromFile(FromFile)
 	for activeIndex < size_s {
 		if s[activeIndex] != '\n' {
-			indexChar := FindCharacterId(rune(s[activeIndex]))
-			PrintCharLine(Charachters,indexChar,line-1)
-			activeIndex++;
-			if activeIndex == size_s  {
-				if line < 8 {
-					line++ ; activeIndex = startIndex
+			if indexChar := FindCharacterId(rune(s[activeIndex])) ; indexChar != -1 {
+				PrintCharLine(Charachters,indexChar,line-1)
+				activeIndex++;
+				if activeIndex == size_s || (activeIndex == size_s - 1 && s[activeIndex]=='\n') {
+					if line < 8 {
+						line++ ; activeIndex = startIndex
+					}
+					fmt.Println()
 				}
-				fmt.Println()
+			} else {
+				activeIndex++
 			}
+			
 		} else {
 			if startIndex == activeIndex || line == 8 {
 				fmt.Println() ; startIndex = activeIndex + 1 ; activeIndex = startIndex ; line = 1
@@ -24,8 +28,7 @@ func PrintCharachters(s, FromFile string) { // "\nHello\n\nworld" // "\n\n\n"   
 }
 func FindCharacterId(c rune) int {
 	index := int(c - 32)
-	if index < 0 {return -1}
-	if index >= 95 {return -2} 
+	if index < 0 || index >= 95 {return -1}
 	return index
 }
 func PrintCharLine(ArrayOfCharchters [95][8]string, index,line int) {
